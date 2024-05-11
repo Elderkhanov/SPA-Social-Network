e4cb56cace2bab35061074a6ef2bce23b55d85cb
Elderkhanov Alan <alan-1995@mail.ru>
2024-05-11 19:58:49 +0300

    initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header className="header">
+      <img src="https://png.pngtree.com/png-clipart/20190604/original/pngtree-creative-company-logo-png-image_1197025.jpg"></img>
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Profile = () => {
+  return (
+    <div className="content">
+      <div>
+        <img src="https://cdn-img1.imgworlds.com/assets/a8f48ba2-9603-4e2b-ac2d-60ce06efa566.jpg?key=home-gallery"></img>
+      </div>
+      <div>
+        <img src="https://www.imgacademy.com/sites/default/files/img-academy-boarding-school-worlds-most-dedicated.jpg"></img>
+      </div>
+      <div>
+        My posts
+        <div>New post</div>
+        <div>post 1</div>
+        <div>post 2</div>
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
